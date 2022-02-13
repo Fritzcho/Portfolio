13f23c0197806c5d69b3d044d67ff8493d618351
alireza <alireza.ramezani@weknowit.se>
2022-02-13 01:13:29 +0100

    first commit

--- sources/script.js
+++ sources/script.js
@@ -27,3 +27,26 @@ function imageClick3() {
 /*        $(document).ready( function() {
             $("#header").load("header.html");
           });*/
+$(document).ready(function(){
+        setTimeout(function () {
+          $("#hero-h1").css('border-right', 'none');
+        }, 4000);
+    });
+$(document).ready(function(){
+        $("#hero-h2").css('opacity', '0');
+        setTimeout(function () {
+            $("#hero-h2").css('opacity', '1');
+        }, 4100);
+    });
+
+$(document).ready(function(){       
+      var scroll_pos = 0;
+      $(document).scroll(function() { 
+          scroll_pos = $(this).scrollTop();
+          if(scroll_pos > 600) {
+              $(".logo").css('color', 'white');
+          } else {
+              $(".logo").css('color', 'black');
+          }
+      });
+  });
