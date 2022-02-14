c075e228a09f99929ade59c472529cfe34994c5c
alireza <alireza.ramezani@weknowit.se>
2022-02-14 22:53:19 +0100

    Updated with more projects

--- sources/script.js
+++ sources/script.js
@@ -23,3 +23,52 @@ $(document).ready(function(){
   });
 });
 
+/*$(document).ready(function(){   
+  $('.project').click(function() {
+    $('.project').css('height', '100vh');
+    $('.project').css('background-color', 'white');
+    $('.project').css('color', 'black');
+    $('.project').css('justify-content', 'left');
+  });
+});*/
+
+let disableScroll = false;
+
+function expand(id) {
+  //Set default open/close settings
+      $('.logo').css('color', 'black !important');
+      $('#'+id).toggleClass('active');
+      $('#'+id+'.p').toggleClass('display');
+      $('html, body').animate({
+        scrollTop: $('#'+id).offset().top-70
+      }, 500);
+      TopScroll = $('#'+id).offset().top-70;
+      LeftScroll = window.pageXOffset;
+
+      // if scroll happens, set it to the previous value
+      if (disableScroll === false) {
+        window.onscroll = function() {
+          window.scrollTo(LeftScroll, TopScroll);
+        };
+        disableScroll = true;
+      } else {
+        window.onscroll = function() {};
+        disableScroll = false;
+      }
+}
+
+$(function() {
+  var rotation = 0, 
+      scrollLoc = $(document).scrollTop();
+  $(window).scroll(function() {
+      var newLoc = $(document).scrollTop();
+      var diff = scrollLoc - newLoc;
+      rotation += diff, scrollLoc = newLoc;
+      var rotationStr = "rotate(" + rotation + "deg)";
+      $(".gear").css({
+          "-webkit-transform": rotationStr,
+          "-moz-transform": rotationStr,
+          "transform": rotationStr
+      });
+  });
+})
